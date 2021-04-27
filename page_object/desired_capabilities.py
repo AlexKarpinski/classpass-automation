71d875a04bae69c65e4f256142f3665241431b45
Alex Karpinski <raptoooor@mail.ru>
2021-04-27 12:47:29 +0300

    update desired_capabilities

--- page_object/desired_capabilities.py
+++ page_object/desired_capabilities.py
@@ -32,7 +32,37 @@ class DesiredCapabilities(object):
         "deviceName": "samsung SM-G920F",
         "appPackage": "com.classpass.classpass",
         "appActivity": "com.classpass.classpass.appstart.viewcontrollers.activities.SplashScreenActivity",
-        "autoGrantPermissions": False,
+        "autoGrantPermissions": True,
+        "locationServicesAuthorized": False,
         "newCommandTimeout": 60,
         "systemPort": 8201
+    }
+
+    KOBITON_SAMSUNG_S6 = {
+        # The generated session will be visible to you only.In case you want this session available for other users, please assign this device to specific group.
+        'sessionName': 'Automation test session',
+        'sessionDescription': '',
+        'deviceOrientation': 'portrait',
+        'noReset': True,
+        'fullReset': False,
+        'captureScreenshots': True,
+        'autoGrantPermissions': False,
+        'locationServicesAuthorized': False,
+        # The maximum size of application is 500MB
+        # By default, HTTP requests from testing library are expired
+        # in 2 minutes while the app copying and installation may
+        # take up-to 30 minutes. Therefore, you need to extend the HTTP
+        # request timeout duration in your testing library so that
+        # it doesn't interrupt while the device is being initialized.
+        'app': 'kobiton-store:188846',
+        'deviceGroup': 'ORGANIZATION',
+        # For deviceName, platformVersion Kobiton supports wildcard
+        # character *, with 3 formats: *text, text* and *text*
+        # If there is no *, Kobiton will match the exact text provided
+        'deviceName': 'Galaxy S6',
+        # The tag is used for finding devices and the user can input only one tag.
+        # For example, the data value will be inputted: tagName="TagName1"
+        'tagName': '',
+        'platformName': 'Android',
+        'platformVersion': '7.0'
     }
