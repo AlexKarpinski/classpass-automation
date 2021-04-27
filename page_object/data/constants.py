a16804a84a9e28035c6d821d28dc65d362618f3d
Alex Karpinski <raptoooor@mail.ru>
2021-04-27 12:47:42 +0300

    update constants

--- page_object/data/constants.py
+++ page_object/data/constants.py
@@ -5,8 +5,4 @@ class Constants:
     WELCOME_LABEL = "For You"
     UNSUCCESSFUL_LOGIN_MESSAGE_TEXT = "The password or email address you entered is incorrect. Reset your password " \
                                       "or contact us if youʼre having trouble logging in."
-    FITNESS = "Fitness"
-    ONLINE = "Online"
-    SPORTS_AND_RECREATION = "Sports and Recreation"
-    CYCLING = "Cycling"
 
